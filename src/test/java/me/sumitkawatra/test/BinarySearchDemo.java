dcdec22e6ed0e0bcb9c1c41e12cd19cab7676408
Devak <Devak>
2016-02-14 18:02:06 +0530

    Not abjectly a test class but a driver class to Run for console output.

---
+++ src/test/java/me/sumitkawatra/test/BinarySearchDemo.java
@@ -0,0 +1,50 @@
+/**
+ * Driver class to execute BinarySearchTree class methods
+ */
+package me.sumitkawatra.test;
+
+import me.sumitkawatra.BinarySearchTree;
+import me.sumitkawatra.TreeNode;
+
+/**
+ * @author Sumit Kawatra
+ *
+ */
+public class BinarySearchDemo {
+
+	/**
+	 * default constructor
+	 */
+	public BinarySearchDemo() {
+		super();
+	}
+	
+	/**
+	 *  Drive demos
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		BinarySearchDemo.demo1();/*smile please*/
+	}
+	
+	/**
+	 * Insert and traverse Demo
+	 */
+	public static void demo1() {
+		BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>();
+		TreeNode<Integer> rootNode = null;
+		
+		rootNode = tree.insert(rootNode, new TreeNode<Integer>(55));
+		rootNode = tree.insert(rootNode, new TreeNode<Integer>(33));
+		rootNode = tree.insert(rootNode, new TreeNode<Integer>(11));
+		rootNode = tree.insert(rootNode, new TreeNode<Integer>(122));
+		rootNode = tree.insert(rootNode, new TreeNode<Integer>(77));
+		rootNode = tree.insert(rootNode, new TreeNode<Integer>(-122));
+		
+		
+		//tree.inOrderTraversal(rootNode);
+		tree.levelOrderTraversal(rootNode);
+		
+		System.out.println("Done demo1");
+	}
+}
