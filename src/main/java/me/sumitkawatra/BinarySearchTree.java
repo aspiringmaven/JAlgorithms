8e18af90626043d288219bcb7ac9f2c280252717
Devak <Devak>
2016-02-14 18:00:16 +0530

    added insert method and levelOrderTraversal method in this class.

--- src/main/java/me/sumitkawatra/BinarySearchTree.java
+++ src/main/java/me/sumitkawatra/BinarySearchTree.java
@@ -3,6 +3,8 @@
  */
 package me.sumitkawatra;
 
+import java.util.LinkedList;
+
 /**
  * @author Sumit Kawatra
  *
@@ -58,6 +60,73 @@ public class BinarySearchTree<T extends Comparable<T>> {
 			postOrderTraversal(root.getLeftChild());
 			postOrderTraversal(root.getRightChild());
 			System.out.println(root);
+		}
+	}
+	
+	/**
+	 * public method to insert node in Binary Search Tree
+	 * @param root
+	 * @param node
+	 * @return
+	 */
+	public TreeNode<T> insert(TreeNode<T> root, TreeNode<T> node) {
+		if (null == node || ( null != node && null == node.getInfo())) {
+			return root;
+		} else if (root == null) { /*First node case*/
+			return node;
+		} else {
+			insertNode(root, node);
+			return root;
+		}
+	}
+	/**
+	 * BST Insert
+	 * @param root
+	 * @param info
+	 * @return
+	 */
+	private void insertNode(TreeNode<T>root,TreeNode<T> node) {
+		if (root.getInfo().compareTo(node.getInfo()) > 0) { /*root is greater*/
+			if(null != root.getLeftChild() ) {
+				insert(root.getLeftChild(), node);
+			} else {
+				root.setLeftChild(node);				
+			}
+		} else if (root.getInfo().compareTo(node.getInfo()) < 0){
+			if(null != root.getRightChild()) {
+				insertNode(root.getRightChild(), node);
+			} else {
+				root.setRightChild(node);
+			}
+		} else {
+			throw new RuntimeException("Node already Exist. "+node.getInfo());
+		}
+	}
+	
+	/**
+	 * levelOrder traversal of Binary Tree non-recursive approach
+	 * @param root
+	 */
+	public void levelOrderTraversal(TreeNode<T> root) {
+		if(null == root) {
+			return;
+		} else {
+			LinkedList<TreeNode<T>> list = new LinkedList<TreeNode<T>>();
+			
+			list.add(root);
+			
+			while(!list.isEmpty()) {
+				TreeNode<T> tempNode = list.removeFirst();
+				System.out.println(tempNode);
+				
+				if(null != tempNode.getLeftChild()) {
+					list.addLast(tempNode.getLeftChild());
+				}
+				 if(null != tempNode.getRightChild()) {
+					 list.addLast(tempNode.getRightChild());
+				 }
+				 tempNode = null; 
+			}
 		}
 	}
 }
